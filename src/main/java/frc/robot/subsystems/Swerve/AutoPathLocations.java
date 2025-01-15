4e7fac33df28b28a7f9362adcc956bce7e57bc36
Ethan McCown <emccown@harding.edu>
2025-01-14 20:43:39 -0600

    Auto Path Loco update

--- src/main/java/frc/robot/subsystems/Swerve/AutoPathLocations.java
+++ src/main/java/frc/robot/subsystems/Swerve/AutoPathLocations.java
@@ -1,19 +1,29 @@
 package frc.robot.subsystems.Swerve;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 
 public enum AutoPathLocations {
-    CORAL_ONE("CORAL_ONE"),
-    CORAL_TWO(""),
-    CORAL_THREE(""),
-    L4("L4 Right #4");
+    CORAL_A(new Pose2d(3.062, 4.2, Rotation2d.fromDegrees(0))),
+    CORAL_B(new Pose2d(3.05, 3.85, Rotation2d.fromDegrees(0))),
+    CORAL_C(new Pose2d(3.55, 2.8, Rotation2d.fromDegrees(57))),
+    CORAL_D(new Pose2d(3.95, 2.65, Rotation2d.fromDegrees(57))),
+    CORAL_E(new Pose2d(5.1, 2.7, Rotation2d.fromDegrees(118))),
+    CORAL_F(new Pose2d(5.4, 2.85, Rotation2d.fromDegrees(118))),
+    CORAL_G(new Pose2d(6.05, 3.85, Rotation2d.fromDegrees(180))),
+    CORAL_H(new Pose2d(5.928, 4.181, Rotation2d.fromDegrees(180))),
+    CORAL_I(new Pose2d(5.353, 5.224, Rotation2d.fromDegrees(-123))),
+    CORAL_J(new Pose2d(5.06, 5.37, Rotation2d.fromDegrees(-123))),
+    CORAL_K(new Pose2d(3.92, 5.361, Rotation2d.fromDegrees(-62))),
+    CORAL_L(new Pose2d(3.627, 5.166, Rotation2d.fromDegrees(-62)));
 
-    private final String path;
+    private final Pose2d coralLocation;
 
-    private AutoPathLocations(String path) {
-        this.path = path;
+    private AutoPathLocations(Pose2d coralLocation) {
+        this.coralLocation = coralLocation;
     }
 
-    public String getPath() {
-        return path;
+    public Pose2d getPath() {
+        return coralLocation;
     }
 }
