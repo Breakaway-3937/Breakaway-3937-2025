0e6d90360ef2420858ab6359da8d13d00d1656b9
Jeffords <rcamferdam@harding.edu>
2025-01-15 13:14:25 -0600

    remove unused imports

--- src/main/java/frc/robot/commands/AutoTeleop.java
+++ src/main/java/frc/robot/commands/AutoTeleop.java
@@ -4,11 +4,9 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.OperatorController;
 import frc.robot.subsystems.Vision;
-import frc.robot.subsystems.Swerve.AutoPathLocations;
 import frc.robot.subsystems.Swerve.Swerve;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
--- src/main/java/frc/robot/subsystems/Swerve/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve/Swerve.java
@@ -34,7 +34,6 @@ import edu.wpi.first.wpilibj.Notifier;
 import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.generated.TunerConstants.TunerSwerveDrivetrain;
