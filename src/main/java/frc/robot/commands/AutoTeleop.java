f098945e90145037082e29724be7bf1f0cae6c93
Jeffords <rcamferdam@harding.edu>
2025-01-16 12:50:42 -0600

    Add new test command

--- src/main/java/frc/robot/commands/AutoTeleop.java
+++ src/main/java/frc/robot/commands/AutoTeleop.java
@@ -5,22 +5,22 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.OperatorController;
 import frc.robot.subsystems.Vision;
 import frc.robot.subsystems.Swerve.Swerve;
 
-// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
-// information, see:
-// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class AutoTeleop extends SequentialCommandGroup {
   private Swerve s_Swerve;
   private Vision s_Vision;
+
   /** Creates a new AutoTelop. */
   public AutoTeleop(Swerve s_Swerve, Vision s_Vision) {
     this.s_Swerve = s_Swerve;
     this.s_Vision = s_Vision;
-    // Add your commands in the addCommands() call, e.g.
-    // addCommands(new FooCommand(), new BarCommand());
-    addCommands(s_Swerve.pathFindToPose(OperatorController.getLocation()));
+
+    addCommands(s_Swerve.pathFindToPose(OperatorController.getPickUpLocation()), 
+                new WaitCommand(5),
+                s_Swerve.pathFindToPose(OperatorController.getScoringLocation()));
   }
 }
