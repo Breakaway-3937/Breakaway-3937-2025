dc8976b399b3c906aa54cc6f8d983aec1c23ef53
Jeffords <rcamferdam@harding.edu>
2025-02-06 08:27:02 -0600

    Remove unused imports

--- src/main/java/frc/robot/commands/AutoTeleop.java
+++ src/main/java/frc/robot/commands/AutoTeleop.java
@@ -7,9 +7,7 @@ package frc.robot.commands;
 import java.util.Set;
 import java.util.function.BooleanSupplier;
 
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.OperatorController;
