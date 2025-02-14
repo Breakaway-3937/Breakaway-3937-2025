47d82bbd414be8952017deeaf965f4dccd76d344
Jeffords <rcamferdam@harding.edu>
2025-02-14 11:01:50 -0600

    remove unused import

--- src/main/java/frc/robot/subsystems/SuperSubsystem.java
+++ src/main/java/frc/robot/subsystems/SuperSubsystem.java
@@ -8,7 +8,6 @@ import java.util.function.BooleanSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.lib.Conversions;
 import frc.robot.subsystems.ClimbAvator.ClimbAvator;
