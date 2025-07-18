b954f920c68b002f72cc52c46202eedcc66fa899
Ethan McCown <emccown@harding.edu>
2025-07-17 20:57:31 -0500

    Added Debugs and fixed offsets (maybe)

--- src/main/java/frc/robot/subsystems/QuestNavSubsystem.java
+++ src/main/java/frc/robot/subsystems/QuestNavSubsystem.java
@@ -13,6 +13,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import gg.questnav.questnav.QuestNav;
 import frc.robot.subsystems.Swerve.Swerve;
@@ -21,7 +22,7 @@ import frc.robot.subsystems.Swerve.Swerve;
 public class QuestNavSubsystem extends SubsystemBase {
 
   public QuestNav questNav = new QuestNav();
-  private Transform2d QUEST_TO_ROBOT = new Transform2d(0.27,0.0, new Rotation2d());
+  private Transform2d QUEST_TO_ROBOT = new Transform2d(34.25,0.0, new Rotation2d());
   private Swerve s_Swerve;
   
   public QuestNavSubsystem(Swerve s_Swerve) {
@@ -60,3 +61,4 @@ public class QuestNavSubsystem extends SubsystemBase {
     updateVisionMeasurement();
   }
 }
+//34.25
